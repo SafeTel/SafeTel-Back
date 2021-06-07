fb0e82ef2172583f6e432b2d087556f106fc20ad
Arthur Boulliard <arthurboulliard2@gmail.com>
2021-06-07 19:10:20 +0200

    [UP] Remove missplaced files

--- tests/DB/atlasBlacklistCollection_test.go
+++
@@ -1,8 +0,0 @@
-//
-// SAFETEL PROJECT, 2021
-// SafeTel-Backend
-// File description:
-// MongoDB-Atlas - atlasBlacklistCollection_test.go
-//
-
-package atlasCollection_test
--- tests/DB/atlasWhitelistCollection_test.go
+++
@@ -1,8 +0,0 @@
-//
-// SAFETEL PROJECT, 2021
-// SafeTel-Backend
-// File description:
-// MongoDB-Atlas - atlasWhitelistCollection_test.go
-//
-
-package atlasCollection_test
