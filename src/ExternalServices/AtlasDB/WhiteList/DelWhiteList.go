ed77d00075dbcfd87da4362751663cde1461f6bf
Arthur Boulliard <arthurboulliard2@gmail.com>
2021-06-07 19:16:39 +0200

    [UP] WhiteList method

---
+++ src/ExternalServices/AtlasDB/WhiteList/DelWhiteList.go
@@ -0,0 +1,29 @@
+//
+// SAFETEL PROJECT, 2021
+// SafeTel-Backend
+// File description:
+// Atlas-Collections - DelWhiteList.go
+//
+
+package whitelistCollections
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson" // Mongo Driver for bson data format
+)
+
+func (h *WhitelistHandler) DelWhiteListPhoneNumberForUserId(userId string, phoneNumber string) {
+	filter := bson.D{{Key: "userId", Value: userId}}
+	update := bson.D{
+		{Key: "$pull", Value: bson.D{ // $pull remove an elem that will match the Value:
+			{Key: "phoneNumbers", Value: phoneNumber}},
+		},
+	} // Want to $push(add) Value into phoneNumbers field
+	deleteResult, err := h.collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Panic("DeleteOne() ERROR:", err)
+	}
+	log.Println("Blacklist DeleteOne Result: number of elem modified: ", deleteResult.ModifiedCount)
+}
