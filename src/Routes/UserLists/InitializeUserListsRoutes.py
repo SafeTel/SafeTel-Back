b649397b1e0ba908b00908e3604acc70c6fb821c
Amaya <guillaume.le-berre@epitech.eu>
2022-01-04 19:05:59 +0100

    [ADD] add call history

--- src/Routes/UserLists/InitializeUserListsRoutes.py
+++ src/Routes/UserLists/InitializeUserListsRoutes.py
@@ -14,6 +14,7 @@ from flask_restful import Api
 from Routes.UserLists.BlackList.AddBlackList import AddBlackList
 from Routes.UserLists.BlackList.DelBlackList import DelBlackList
 from Routes.UserLists.BlackList.GetBlackList import GetBlackList
+from Routes.UserLists.History.AddHistory import AddHistory
 from Routes.UserLists.History.DelHistory import DelHistory
 from Routes.UserLists.History.GetHistory import GetHistory
 from Routes.UserLists.WhiteList.AddWhiteList import AddWhiteList
@@ -26,10 +27,12 @@ def InitializeUserListsRoutes(api):
     api.add_resource(DelBlackList, "/user/blacklist")
     api.add_resource(GetBlackList, "/user/blacklist")
 
-    api.add_resource(GetHistory, "/user/history")
+    api.add_resource(AddHistory, "/user/history")
     api.add_resource(DelHistory, "/user/history")
-    api.add_resource(AddWhiteList, "/user/whitelist")
+    api.add_resource(GetHistory, "/user/history")
 
+    api.add_resource(AddWhiteList, "/user/whitelist")
     api.add_resource(DelWhiteList, "/user/whitelist")
     api.add_resource(GetWhiteList, "/user/whitelist")
+
     api.add_resource(GetGreyList, "/user/greylist")
