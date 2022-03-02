ad80009c6c029ee85d099417af2bbad7935a1227
Amaya <guillaume.le-berre@epitech.eu>
2022-03-01 21:42:23 -0500

    [REFACTO] regex caching service

---
+++ src/Logic/Services/RegexCachingService/RegexService.py
@@ -0,0 +1,22 @@
+##
+## EPITECH PROJECT, 2022
+## SafeTel-Back
+## File description:
+## RegexService
+##
+
+import re
+
+class RegexCachingService():
+    def __init__(self):
+        self.__cache = {}
+
+    def GetOrCompileAndCache(self, name: str, pattern: str):
+        if (name in self.__cache):
+            return self.__cache[name]
+        return self.__CompileAndCache(name, pattern)
+
+    def __CompileAndCache(self, name: str, pattern: str):
+        compiled = re.compile(pattern)
+        self.__cache[name] = compiled
+        return compiled
