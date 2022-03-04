15a31e270a940bc26adc1cd6864a9c9e9dcca45f
Amaya <guillaume.le-berre@epitech.eu>
2022-03-03 23:16:23 -0500

    [REFACTO] implements resolver & lists at high lvl usage

--- src/Infrastructure/Factory/UserFactory/Lists/Blacklist.py
+++ src/Infrastructure/Factory/UserFactory/Lists/Blacklist.py
@@ -5,17 +5,36 @@
 ## Blacklist
 ##
 
-# Lists db imports
+### INFRA
+# Blacklist db import
 from Infrastructure.Services.MongoDB.Melchior.UserLists.BlackListDB import BlacklistDB
+# Conflict Resolver for number in Lists
+from Infrastructure.Factory.UserFactory.Lists.NumberConflictResolver import NumberConflictResolver
 
+### MODELS
+# PhoneList Model import
+from Models.Infrastructure.Factory.UserFactory.Lists.PhoneList import PhoneList
+
+# Represents Blacklist at high level usage
 class Blacklist():
-    def __init__(self, guid: str, BlacklistDB: BlacklistDB):
+    def __init__(self, guid: str, BlacklistDB: BlacklistDB, ConflictResolver: NumberConflictResolver):
         self.__guid = guid
         self.__BlacklistDB = BlacklistDB
+        self.__ConflictResolver = ConflictResolver
+
 
     def PullList(self):
-        return self.__BlacklistDB.getBlacklistForUser(self.__guid)
+        return PhoneList(self.__BlacklistDB.getBlacklistForUser(self.__guid))
+
 
     def AddNumber(self, number: str):
-        self.__BlacklistDB.addBlacklistNumberForUser(number)
-        return self.__BlacklistDB.getBlacklistForUser(self.__guid)
+        if (self.__ConflictResolver.IsConflict(number)):
+            self.__ConflictResolver.ResolveIntoBlacklist(number)
+        else:
+            self.__BlacklistDB.addBlacklistNumberForUser(number)
+        return PhoneList(self.__BlacklistDB.getBlacklistForUser(self.__guid))
+
+
+    def DeleteNumber(self, number: str):
+        self.__BlacklistDB.delBlacklistNumberForUser(self.__guid, number)
+        return PhoneList(self.__BlacklistDB.getBlacklistForUser(self.__guid))
---
+++ src/Infrastructure/Factory/UserFactory/Lists/Whitelist.py
@@ -0,0 +1,40 @@
+##
+## EPITECH PROJECT, 2022
+## SafeTel-Back
+## File description:
+## Whitelist
+##
+
+### INFRA
+# Blacklist db import
+from Infrastructure.Services.MongoDB.Melchior.UserLists.WhiteListDB import WhitelistDB
+# Conflict Resolver for number in Lists
+from Infrastructure.Factory.UserFactory.Lists.NumberConflictResolver import NumberConflictResolver
+
+### MODELS
+# PhoneList Model import
+from Models.Infrastructure.Factory.UserFactory.Lists.PhoneList import PhoneList
+
+# Represents Blacklist at high level usage
+class Blacklist():
+    def __init__(self, guid: str, WhitelistDB: WhitelistDB, ConflictResolver: NumberConflictResolver):
+        self.__guid = guid
+        self.__WhitelistDB = WhitelistDB
+        self.__ConflictResolver = ConflictResolver
+
+
+    def PullList(self):
+        return PhoneList(self.__WhitelistDB.getWhitelistForUser(self.__guid))
+
+
+    def AddNumber(self, number: str):
+        if (self.__ConflictResolver.IsConflict(number)):
+            self.__ConflictResolver.ResolveIntoWhitelist(number)
+        else:
+            self.__WhitelistDB.addWhitelistNumberForUser(number)
+        return PhoneList(self.__WhitelistDB.getWhitelistForUser(self.__guid))
+
+
+    def DeleteNumber(self, number: str):
+        self.__WhitelistDB.delWhitelistNumberForUser(self.__guid, number)
+        return PhoneList(self.__WhitelistDB.getWhitelistForUser(self.__guid))
