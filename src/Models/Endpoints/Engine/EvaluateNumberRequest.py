69d1be8392dcf1422de32201c401d5f8b13e1159
Amaya <guillaume.le-berre@epitech.eu>
2022-03-24 17:39:16 -0400

    [ADD] evaluate number endpoint

--- src/Models/Endpoints/Engine/EvaluateNumberRequest.py
+++ src/Models/Endpoints/Engine/EvaluateNumberRequest.py
@@ -1,8 +1,8 @@
 ##
-## SAFETEL PROJECT, 2022
+## EPITECH PROJECT, 2022
 ## SafeTel-Back
 ## File description:
-## TellowsRequest
+## EvaluateNumberRequest
 ##
 
 ### MODELS
@@ -10,14 +10,14 @@
 from Models.ModelAbstractions.JParent import JParent
 
 # Represents Tellows Request
-class TellowsRequest(JParent):
+class EvaluateNumberRequest(JParent):
     def __init__(self, rawJSON: str):
         loadedJSON = self.Load(rawJSON)
         self.__InitJParent(loadedJSON)
 
     # Values Assignement
     def __InitJParent(self, loadedJSON: dict):
-        self.magicnumber = self.LoadElement(loadedJSON, "magicnumber")
+        self.token = self.LoadElement(loadedJSON, "token")
         self.number = self.LoadElement(loadedJSON, "number")
 
     # Errors Evaluation
@@ -27,9 +27,9 @@ class TellowsRequest(JParent):
         return None
 
     def __EvaErrorsJParent(self):
-        if (self.magicnumber is None): return "Body Denied"
-        if (type(self.magicnumber) is not int and self.magicnumber != 42): return "Token Denied"
+        if (self.token is None): return "Body Denied"
+        if (type(self.token) is not str): return "Token Denied"
 
         if (self.number is None): return "Body Denied"
-        if (type(self.number) is not str): return "Noken Denied"
+        if (type(self.number) is not str): return "Number Denied"
         return None
---
+++ src/Models/Endpoints/Engine/EvaluateNumberResponse.py
@@ -0,0 +1,30 @@
+##
+## EPITECH PROJECT, 2022
+## SafeTel-Back
+## File description:
+## EvaluateNumberResponse
+##
+
+### MODELS
+# Abstraction import
+from Models.ModelAbstractions.JParent import JParent
+
+# Represents Tellows Response
+class EvaluateNumberResponse(JParent):
+    def __init__(self, block: bool):
+        self.__InitJParent(block)
+
+    # Values Assignement
+    def __InitJParent(self, block: bool):
+        self.block = block
+
+    # Errors Evaluation
+    def EvaluateModelErrors(self):
+        errorJParent = self.__EvaErrorsJParent()
+        if (errorJParent != None): return errorJParent
+        return None
+
+    def __EvaErrorsJParent(self):
+        if (self.block is None): return "Internal Model Error"
+        if (type(self.block) is not bool): return "Internal Model Error"
+        return None
