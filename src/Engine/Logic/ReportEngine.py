b44e4da12920f89ea3c702715f6baf4e09ad4569
Amaya <guillaume.le-berre@epitech.eu>
2022-04-27 16:37:34 -0400

    [ADD] init engine

---
+++ src/Engine/Logic/ReportEngine.py
@@ -0,0 +1,21 @@
+##
+## SAFETEL PROJECT, 2022
+## SafeTel-Back
+## File description:
+## Engine
+##
+
+### INFRA
+# User sub class import
+from Infrastructure.Factory.UserFactory.User import User
+
+class ReportEngine(self):
+    def __init__():
+        return
+
+
+    def Process(self, User: User, number: str):
+        # TODO: evealuate the number
+        # TODO: if malicious number block the number for the user & report it
+        # TODO: else just dont block it
+        return
