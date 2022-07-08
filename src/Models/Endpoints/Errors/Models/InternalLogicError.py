c3f3894c039022f04a3c2b62e6a10b4f99255bdd
Arthur Boulliard <arthurboulliard2@gmail.com>
2022-07-08 11:08:39 +0200

    [UP] removing logging methods

--- src/Models/Endpoints/Errors/Models/InternalLogicError.py
+++ src/Models/Endpoints/Errors/Models/InternalLogicError.py
@@ -11,8 +11,3 @@ from Models.Endpoints.Errors.Models.EObject import EObject
 class InternalLogicError(EObject):
     def __init__(self, loadedJSON: dict):
         super().__init__(loadedJSON)
-        self.__LogingError()
-
-    def __LogingError(self):
-        ## TODO: waiting for logging class
-        return
