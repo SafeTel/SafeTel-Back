c0841d327379838e88a7eaa79a08e7800ad8f1a4
Arthur Boulliard <arthurboulliard2@gmail.com>
2022-11-19 22:40:36 +0100

    [HOTFIX] for deployment scripts

--- devOps/scripts/deploy-magi.sh
+++ devOps/scripts/deploy-magi.sh
@@ -10,17 +10,11 @@ else
     echo "You are going to deploy magi_$1"
 fi
 
-echo 'If you are ready for the deploy, please trigger Enter'
-read MYSQL_PRESENT
+echo "Deploying $1 with absolute path $2/cd"
 
-echo 'devOps absolute path parent folder:'
-read DOCKER_DIRECTORY
-
-echo "Deploying $1"
-
-if [ ! -d "${DOCKER_DIRECTORY}/cd" ]; then
-        echo "${DOCKER_DIRECTORY}  does not exists or does not contains docker-compose folder"
+if [ ! -d "$2/cd" ]; then
+        echo "$2  does not exists or does not contains docker-compose folder"
         exit 1
 fi
 
-cd $DOCKER_DIRECTORY/cd && docker-compose pull magi_$1 && docker-compose up -d --force-recreate --no-deps --build magi_$1
+cd $2/cd && docker-compose pull magi_$1 && docker-compose up -d --force-recreate --no-deps --build magi_$1
